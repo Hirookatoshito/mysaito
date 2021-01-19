52bcd976f22bcce162f9993481623867b29842ba
Hirookatoshito <74745265+Hirookatoshito@users.noreply.github.com>
2021-01-19 11:49:22 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,12 +8,11 @@
   {
     let imgArray = document.querySelectorAll(className);
     if (slideNo >= 0) {
-      //初回以外は現在のスライドを消す
       imgArray[slideNo].style.opacity = 0;
     }
     slideNo++;
     if (slideNo >= imgArray.length) {
-      slideNo = 0; //次のスライドがなければ最初のスライドへ戻る
+      slideNo = 0;
     }
     imgArray[slideNo].style.opacity = 1;
     setTimeout(function(){viewSlide(className, slideNo);}, 6000);
@@ -26,11 +25,17 @@
     }
   }
 
+  window.addEventListener('load', () => {
+    showTopAnimation();
+    topTextMove();
+  })
+
   let menu = document.querySelector('.header-nav');
   let closeText = document.querySelector('.close')
   let menubar = document.querySelector('.menu-trigger');
   let open = document.querySelector('.open');
   let headerText = document.querySelector('.header-nav-text');
+
   menu.addEventListener('click', () => {
     menubar.classList.toggle('active');
     open.classList.toggle('show');
@@ -41,22 +46,26 @@
     open.classList.remove('show');
     headerText.classList.remove('hide');
   })
-  
-  function newsMove() {
-    let newsTop = document.querySelector('.news-top');
-    newsTop.classList.add('move')
-  }
 
-  window.addEventListener('load', () => {
-    showTopAnimation();
-    newsMove();
-  })
+
+  let scrollY = window.pageYOffset;
+  let windowH = window.innerHeight;
+
+
+  function topTextMove() {
+    let topText = document.querySelectorAll('.top-text');
+    for (let i = 0; i < topText.length; i++) {
+      let elemClientRect = topText[i].getBoundingClientRect();
+      let elemY = scrollY + elemClientRect.top;
+      if (scrollY + windowH > elemY) {
+        topText[i].classList.add('move')
+      }
+    }
+  }
+  window.addEventListener('scroll', topTextMove);
 
   function newsPostMove() {
     let newsPosts = document.querySelectorAll('.news-post');
-    let scrollY = window.pageYOffset;
-    let windowH = window.innerHeight;
-
     for(let i = 0; i < newsPosts.length; i++) {
       let elemClientRect = newsPosts[i].getBoundingClientRect();
       let elemY = scrollY + elemClientRect.top;
@@ -66,11 +75,50 @@
     }
   }
   window.addEventListener('scroll', newsPostMove);
-      
-
-
-
-
-
   
+  function conceptTopMove() {
+    let conceptH2 = document.querySelector('.concept-h2');
+    let elemClientRect = conceptH2.getBoundingClientRect();
+    let elemY = scrollY + elemClientRect.top;
+    if(scrollY + windowH > elemY) {
+      conceptH2.classList.add('move')
+    }
+  }
+  window.addEventListener('scroll', conceptTopMove);
+
+  function underMove() {
+    let underContent = document.querySelectorAll('.undermove');
+    for (let i = 0; i < underContent.length; i++) {
+      let elemClientRect = underContent[i].getBoundingClientRect();
+      let elemY = scrollY + elemClientRect.top;
+      if (scrollY + windowH > elemY) {
+        underContent[i].classList.add('move')
+      }
+    }
+  }
+  window.addEventListener('scroll', underMove);  
+
+  function leftMove() {
+    let leftContent = document.querySelectorAll('.leftmove');
+    for (let i = 0; i < leftContent.length; i++) {
+      let leftClientRect = leftContent[i].getBoundingClientRect();
+      let leftY = scrollY + leftClientRect.top;
+      if (scrollY + windowH > leftY) {
+        leftContent[i].classList.add('move')
+      }
+    }
+  }
+  window.addEventListener('scroll', leftMove);  
+
+  function rightMove() {
+    let rightContent = document.querySelectorAll('.rightmove');
+    for (let i = 0; i < rightContent.length; i++) {
+      let rightClientRect = rightContent[i].getBoundingClientRect();
+      let rightY = scrollY + rightClientRect.top;
+      if (scrollY + windowH > rightY) {
+        rightContent[i].classList.add('move')
+      }
+    }
+  }
+  window.addEventListener('scroll', rightMove);  
 }
